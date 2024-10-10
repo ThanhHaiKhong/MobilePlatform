ff0ce765a6baca4c3dff49e0b9167bb6ee62e9d9
Thanh Hai Khong <khongthanhhai@gmail.com>
2024-10-10 15:39:00 +0700

    SettingsView embeded in NavigationStack

--- Sources/MobilePlatformUI/SettingsView/SettingsView.swift
+++ Sources/MobilePlatformUI/SettingsView/SettingsView.swift
@@ -16,51 +16,53 @@ public struct SettingsView: View {
     }
     
     public var body: some View {
-        List {
-            Section {
-                SettingItemView(image: "square.and.arrow.up", title: "Share App", backgroundColor: .blue)
-                    .onTapGesture {
-                        store.send(.shareButtonTapped)
-                    }
+        NavigationStack {
+            List {
+                Section {
+                    SettingItemView(image: "square.and.arrow.up", title: "Share App", backgroundColor: .blue)
+                        .onTapGesture {
+                            store.send(.shareButtonTapped)
+                        }
+                    
+                    SettingItemView(image: "star.fill", title: "Rate App", backgroundColor: .yellow)
+                        .onTapGesture {
+                            store.send(.rateButtonTapped)
+                        }
+                }
+                .listRowBackground(BlurView(style: .systemChromeMaterial))
                 
-                SettingItemView(image: "star.fill", title: "Rate App", backgroundColor: .yellow)
-                    .onTapGesture {
-                        store.send(.rateButtonTapped)
-                    }
+                Section {
+                    SettingItemView(image: "paperplane.fill", title: "Contact the Team", subtitle: "We'd love to hear from you.", backgroundColor: .green)
+                        .onTapGesture {
+                            store.send(.contactButtonTapped)
+                        }
+                    
+                    SettingItemView(image: "exclamationmark.bubble.fill", title: "Report a Problem", subtitle: "Please let us know if you encounter any issues.", backgroundColor: .black)
+                        .onTapGesture {
+                            store.send(.reportButtonTapped)
+                        }
+                    
+                    SettingItemView(image: "star.bubble.fill", title: "Request a Feature", subtitle: "Don't hesitate to share your ideas with us.", backgroundColor: .red)
+                        .onTapGesture {
+                            store.send(.requestButtonTapped)
+                        }
+                }
+                .listRowBackground(BlurView(style: .systemChromeMaterial))
+                
+                Section {
+                    SettingItemView(image: "doc.richtext.fill", title: "Privacy Policy", backgroundColor: .purple)
+                        .onTapGesture {
+                            store.send(.privacyPolicyButtonTapped)
+                        }
+                    
+                    SettingItemView(image: "doc.plaintext.fill", title: "Term of Use", backgroundColor: .orange)
+                        .onTapGesture {
+                            store.send(.termsOfServiceButtonTapped)
+                        }
+                }
+                .listRowBackground(BlurView(style: .systemChromeMaterial))
             }
-            .listRowBackground(BlurView(style: .systemChromeMaterial))
-            
-            Section {
-                SettingItemView(image: "paperplane.fill", title: "Contact the Team", subtitle: "We'd love to hear from you.", backgroundColor: .green)
-                    .onTapGesture {
-                        store.send(.contactButtonTapped)
-                    }
-                
-                SettingItemView(image: "exclamationmark.bubble.fill", title: "Report a Problem", subtitle: "Please let us know if you encounter any issues.", backgroundColor: .black)
-                    .onTapGesture {
-                        store.send(.reportButtonTapped)
-                    }
-                
-                SettingItemView(image: "star.bubble.fill", title: "Request a Feature", subtitle: "Don't hesitate to share your ideas with us.", backgroundColor: .red)
-                    .onTapGesture {
-                        store.send(.requestButtonTapped)
-                    }
-            }
-            .listRowBackground(BlurView(style: .systemChromeMaterial))
-            
-            Section {
-                SettingItemView(image: "doc.richtext.fill", title: "Privacy Policy", backgroundColor: .purple)
-                    .onTapGesture {
-                        store.send(.privacyPolicyButtonTapped)
-                    }
-                
-                SettingItemView(image: "doc.plaintext.fill", title: "Term of Use", backgroundColor: .orange)
-                    .onTapGesture {
-                        store.send(.termsOfServiceButtonTapped)
-                    }
-            }
-            .listRowBackground(BlurView(style: .systemChromeMaterial))
+            .navigationTitle("Settings")
         }
-        .navigationTitle("Settings")
     }
 }
